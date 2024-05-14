17c93e096d6489735e84b0caf8340bfb67d725a1
Kai Windle <kaiwindle@gmail.com>
2024-05-14 15:58:37 +0100

    update

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
             'mp3_converter=converter:main',
         ],
     },
-    author='Your Name',
+    author='Kai Windle',
     author_email='kaiwindle@gmail.com',
     description='Convert MP3 files to OGG and FLAC formats.',
     url='https://github.com/Drakx/mp3_converter',
